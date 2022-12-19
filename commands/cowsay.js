e63e41ac1b13c6081807c65615f938f8224c0b43
RadicaIEdward <edward.o.galindez@gmail.com>
2022-12-18 22:13:27 -0500

    Update cowsay to take user input

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -1,18 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
 const cowsay = require('cowsay');
 
-const moo = cowsay.say({
-  text: 'Moo!',
-  e: 'oO',
-  T: 'U ',
-  f: 'cat2',
-}).replaceAll('`', "'");
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('cowsay')
-    .setDescription('The cow speaks!'),
+    .setDescription('The cow speaks!')
+    .addStringOption((option) => option.setName('input')
+      .setDescription('Tell the cow what to say!')
+      .setMaxLength(2000)
+      .setRequired(true)),
+  // .addStringOption((option) => option.setName('cow')
+  //   .setDescription('Choose your cow!')
+  //   .setMaxLength(2000)),
   async execute(interaction) {
+    const input = interaction.options.getString('input');
+    const moo = cowsay.say({
+      text: input,
+      e: 'oo',
+      T: 'U ',
+      f: 'cat2',
+    }).replaceAll('`', "'");
+    // const cowChoice = interaction.options.getString('input');
     await interaction.reply(`\`\`\` ${moo}\`\`\``);
   },
 };
